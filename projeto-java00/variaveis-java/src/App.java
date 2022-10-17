76fab56993b8c9368db604655091d42049f2974e
Joely <sjoely131@gmail.com>
2022-10-16 22:39:20 -0300

    ok

--- projeto-java00/variaveis-java/src/App.java
+++ projeto-java00/variaveis-java/src/App.java
@@ -1,5 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //TIPOS PRIMITIVOS
+        //ESTUDAR CLASSE DE STRING
+
+
+        double salarioMinimo = 2500.33;
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+
+        //deixar uma variavel com o mesmo valor coloca final, ai não importa o valor n muda
+        final int NUMERO = 5;
+        System.out.println(NUMERO);
     }
 }
